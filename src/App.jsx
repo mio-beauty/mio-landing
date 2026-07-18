db7095344017589a5cada0686b0f63d54a24e0a1
mirjack <mirjalolbekkosimov@gmail.com>
2026-07-18 09:04:31 -0700

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Analytics />
+    </>
+  );
 }
